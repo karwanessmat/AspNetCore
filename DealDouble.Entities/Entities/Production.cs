89bdddefee37340bbc52128eff652f6057a55a6b
karwanessmat <42093729+karwanessmat@users.noreply.github.com>
2020-01-29 00:45:59 +0300

    Add Enitities Project

---
+++ DealDouble.Entities/Entities/Production.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DealDouble.Entities.Entities
+{
+    class Production
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public decimal ActualAmount { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+    }
+}
